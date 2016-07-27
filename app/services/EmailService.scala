1a9de16f1703d1fd5005ead9c5fc30a4f849cfa1
Valera Rusakov <valera.rusakov@gmail.com>
2016-07-27 23:58:42 +0300

    changed email

--- app/services/EmailService.scala
+++ app/services/EmailService.scala
@@ -28,7 +28,7 @@ class EmailService @Inject()(mailerClient: MailerClient,
     val messageBody = Messages("email.registration.body", firstName, lastName, email)
     val mail = Email(Messages("email.registration.title"),
       noReplyEmail,
-      Seq("valera.rusakov@gmail.com"),
+      Seq("yegormakarov@gmail.com"),
       bodyHtml = Option(messageBody))
     mailerClient.send(mail)
   }
