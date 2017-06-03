02f127ad4e653e4a4f39f75f99474b6c50cba5f4
Valera Rusakov <valera.rusakov@gmail.com>
2017-06-03 23:11:17 +0300

    Remove authorization for bookings rest request

--- app/controllers/rest/BookingsController.scala
+++ app/controllers/rest/BookingsController.scala
@@ -4,6 +4,7 @@ import com.google.inject.Inject
 import controllers.rest.base._
 import dto.rest.BookingDtos.BookingDayDto
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc.Action
 import security.TokenStorage
 import services.internal.bookings.BookingService
 
@@ -11,7 +12,7 @@ import services.internal.bookings.BookingService
 class BookingsController @Inject()(val tokenStorage: TokenStorage,
                                    bookingService: BookingService) extends BaseController {
 
-  def getBookingSlots(version: String) = authorized.async { _ =>
+  def getBookingSlots(version: String) = Action.async { _ =>
     bookingService.getBookingSlots()
       .map(BookingDayDto.fromBookingSlots)
       .map(bookingDays => ok(bookingDays))
