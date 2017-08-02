34a6318db5495e2e3d98d8020bbe1cae18df4b72
Valera Rusakov <valera.rusakov@gmail.com>
2017-08-02 23:00:40 +0300

    Update service description

---
+++ conf/evolutions/default/18.sql
@@ -0,0 +1,7 @@
+UPDATE services
+SET description = 'Qweex Waterless Wash® <br/>' ||
+                  'Spray-On Carnauba Wax <br/>' ||
+                  'Wheels and Tires <br/>' ||
+                  'No-Grease Tire Shine <br/>' ||
+                  'Windows'
+WHERE key = 'EXTERIOR_CLEANING';
